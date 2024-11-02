1d6a36cea59f23808b896a12eeb4b08939bdba3d
JustinG <justin.gestiere@gmail.com>
2024-11-02 13:38:20 +0100

    entreprise

--- entreprise.php
+++ entreprise.php
@@ -20,9 +20,57 @@
           </div>
         </div>
         <div class="box">
+          <div class="detail-box">
+            <h5>
+              Contexte  
+            </h5>
+            <p>
+              La Sécurité Sociale en France est un système de protection sociale qui a été créé après la Seconde Guerre mondiale 
+              pour garantir aux citoyens un accès à des soins de santé, des prestations familiales, des allocations chômage et des 
+              pensions de retraite. Elle est divisée en plusieurs branches dont l'Assurance maladie : C'est l'une des branches principales,
+              couvrant les frais de santé. Elle rembourse une partie des dépenses de santé et est divisée également en plusieurs organismes
+              dont les CTI (Centre de Traitement Informatique).
+            </p>
+          </div>
+        </div>
+        <div class="box">
           <div class="img-box-page">
             <img src="images/entreprise.png" alt="">
           </div>
+          <div class="detail-box">
+            <h5>
+              Qu'est-ce qu'un Centre de Traitement Informatique (CTI) ?
+            </h5>
+            <p>
+              Un Centre de Traitement Informatique (CTI) est une structure dédiée à la gestion et au traitement des données dans le 
+              secteur de la santé. Il joue un rôle crucial dans la mise en œuvre des systèmes d'information liés à la Sécurité Sociale. 
+              Les CTI sont responsables du traitement des dossiers médicaux, de la gestion des remboursements de soins, et de l'analyse 
+              des données de santé. Les CTI assurent la centralisation et l'intégration des informations provenant de divers acteurs de 
+              la santé, tels que les hôpitaux, les médecins, et les établissements de soins. Leur mission inclut également la sécurité 
+              des données et le respect de la confidentialité des informations médicales. En collaborant avec les caisses de sécurité 
+              sociale et d'autres institutions, les CTI facilitent l'accès aux données, améliorent la qualité des soins, et contribuent 
+              à une gestion efficace des ressources de santé.
+            </p>
+          </div>
+        </div>
+        <div class="box">
+          <div class="detail-box">
+            <h5>
+              Mon rôle au sein du CTI Angers
+            </h5>
+            <p>
+              Le CTI Angers compte différent groupe et type de métier. Au sein de mon entreprise je fais parti des intégrateurs 
+              d'exploitation qui jouent un rôle clé dans l'optimisation et la gestion des systèmes d'information au sein 
+              de l'assurance maladie. Ils sont chargés d'intégrer divers systèmes, logiciels et équipements afin de garantir une 
+              communication fluide et d'améliorer l'efficacité des différents outils.
+              En somme, les intégrateurs d'exploitation sont en capacité à concevoir, intégrer et maintenir des systèmes d'information 
+              performants ce qui est cruciale pour garantir la compétitivité et l'agilité des organisations dans un monde de plus en plus 
+              axé sur la technologie. Ils contribuent à créer un écosystème numérique harmonieux qui soutient les objectifs stratégiques 
+              de l'entreprise et améliore l'expérience utilisateur globale.
+            </p>
+          </div>
+        </div>
+        <div class="box">
           <div class="detail-box">
             <h5>
               Compétences mises en oeuvre 
@@ -30,6 +78,16 @@
             <p>
               <pre>
 
+<strong>- Gérer le patrimoine informatique</strong>
+Recenser et identifier les ressources nimériques
+Exploiter des référentiels, normes et standards adoptés par le prestataire informatique
+Mettre en place et vérifier les niveaux d'habilitation associés à un service
+Vérifier les conditions de la continuité d'un service informatique
+Gérer les sauvegardes
+<strong>- Répondre aux incidents et aux demandes d'assistance et d'évolution</strong>
+Collecter, suivre et orienter des demandes
+Traiter des demandes concernant les services réseau et système applicatifs
+Traiter des demandes concernant les applications
 <strong>- Travailler en mode projet</strong>
 Analyser les objectifs et les modalités d'organisation d'un projet
 Planifier les activités
@@ -37,73 +95,10 @@ Planifier les activités
 Réaliser les tests d'intégration et d'acceptation d'un service
 Déployer un service
 Accompagner les utilisateurs dans la mise en place d'un service
-              </pre>
-            </p>
-          </div>
-        </div>
-        <div class="box">
-          <div class="detail-box">
-            <h5>
-              Contexte  
-            </h5>
-            <p>
-              L'objectif initial était de concevoir une calculatrice immobilière entièrement fonctionnelle, bâtie de A à Z, en mettant en œuvre 
-              des compétences en développement web. Pour ce faire, il a fallu combiner plusieurs langages de programmation et technologies, 
-              notamment HTML, CSS et JavaScript, chacun jouant un rôle clé dans le processus de création.
-              Le HTML a permis de structurer la calculatrice immobilière en définissant les éléments comme les boutons et champs d'entrée. 
-              Le CSS a servi à styliser l'interface, en ajustant la mise en page et en rendant l'application responsive. Le JavaScript a géré 
-              les fonctionnalités interactives, réalisant les calculs en temps réel et affichant les résultats.
-            </p>
-          </div>
-        </div>
-        <div class="box">
-          <div class="detail-box">
-            <h5>
-              Description des différentes tâches  
-            </h5>
-            <pre>
-- Étude de la maquette Figma
-- Réalisation de la partie HTML et CSS afin d'avoir l'aperçu de la page web
-- Réalisation de la partie JavaScript pour rendre la page dynamique
-- Mise en place d'un bouton PDF
-- Rendre la page responsive pour que les appareils portables puissent avoir accès au site
-            </pre>
-          </div>
-        </div>
-        <div class="box">
-          <div class="detail-box">
-            <h5>
-              Outils utilisés
-            </h5>
-            <p>
-              <pre>
-- Logiciel Visual Studio Code ainsi que certaines extensions du logiciel
-- Site internet Microlead pour les cours HTML, CSS et JavaScript
-- Editeur graphique Figma permettant l'affichage du modèle à suivre
-              </pre>
-            </p>
-          </div>
-        </div>
-        <div class="box">
-          <div class="detail-box">
-            <h5>
-              Collaborateurs
-            </h5>
-            <p>
-              Travail réalisé individuellement avec l'assistance de notre professeur.
-            </p>
-          </div>
-        </div>
-        <div class="box">
-          <div class="detail-box">
-            <h5>
-              Informations supplémentaires
-            </h5>
-            <p>
-              <pre>
-Effectué au sein de l'établissement : My Digital School à Angers
-Option : Services informatiques aux organisations
-Période : Du 15 Juin 2024 au 1er Septembre 2024
+<strong>- Organiser son développement professionnel</strong>
+Mettre en place son environnement d'apprentissage personnel
+Gérer son identité professionnel
+Développer son projet professionnel
               </pre>
             </p>
           </div>
