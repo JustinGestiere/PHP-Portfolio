98b83bba52abd39ab17ec4db778d5bafb0c0947d
root <root@vps109215.serveur-vps.net>
2025-02-28 10:50:52 +0100

    test

--- entreprise.php
+++ entreprise.php
@@ -8,7 +8,7 @@
     <div class="container">
       <div class="heading_container heading_center">
         <h2>
-          Mon <span>entreprise</span>
+          Montest <span>entreprise</span>
         </h2>
       </div>
       <div class="why_container">
