8a73e329d3d82f2d2b13303a7c064d7ba4b1b574
JustinG <justin.gestiere@gmail.com>
2025-04-24 16:40:54 +0200

    please

--- page_réalisation3.php
+++ page_réalisation3.php
@@ -175,13 +175,13 @@ Période : De Février 2025 à Avril 2025
             <div class="img-boxe">
               <a href="https://clickneat.justingestiere.com/login" target="_blank"><img src="images/lien_site.png" alt=""></a>
             </div>
-            <a href="https://clickneat.justingestiere.com/login" target="_blank"><p>Adresse du site web</p></a>
+            <a href="https://clickneat.justingestiere.com/login" target="_blank"><p>Adresse du site web : https://clickneat.justingestiere.com/login</p></a>
           </div>
           <div class="ligne-boxe">
             <div class="img-boxe">
               <a href="https://github.com/JustinGestiere/LARAVEL_Restaurant" target="_blank"><img src="images/logo_git.png" alt=""></a>
             </div>
-            <a href="https://github.com/JustinGestiere/LARAVEL_Restaurant" target="_blank"><p>Repository GitHub</p></a>
+            <a href="https://github.com/JustinGestiere/LARAVEL_Restaurant" target="_blank"><p>Repository GitHub : https://github.com/JustinGestiere/LARAVEL_Restaurant</p></a>
           </div>
           <div class="ligne-boxe">
             <div class="img-boxe">
